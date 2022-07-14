cf400913adda100ae3a2ea3f873b970e70ebf494
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-07-15 03:07:37 +0530

    Add treesitter refactor plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,24 +5,26 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   -- colorscheme
   use 'joshdick/onedark.vim'
+  -- devicons
+  use 'kyazdani42/nvim-web-devicons'
+  -- statusline
+  use 'nvim-lualine/lualine.nvim'
+  -- tabline
+  use { 'akinsho/bufferline.nvim', tag = "v2.*" }
+  -- toggleterm
+  use { "akinsho/toggleterm.nvim", tag = 'v2.*' }
+  -- gitsigns
+  use 'lewis6991/gitsigns.nvim'
   -- LSP config
   use 'neovim/nvim-lspconfig'
   -- LSP Installer
   use "williamboman/nvim-lsp-installer"
   -- LSP diagnostics
   use 'jose-elias-alvarez/null-ls.nvim'
-  -- treesitter
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  -- statusline
-  use 'nvim-lualine/lualine.nvim'
-  -- devicons
-  use 'kyazdani42/nvim-web-devicons'
-  -- tabline
-  use { 'akinsho/bufferline.nvim', tag = "v2.*" }
   -- diagnostics
   use 'folke/trouble.nvim'
-  -- gitsigns
-  use 'lewis6991/gitsigns.nvim'
-  -- toggleterm
-  use { "akinsho/toggleterm.nvim", tag = 'v2.*' }
+  -- treesitter
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  -- treesitter refactor
+  use 'nvim-treesitter/nvim-treesitter-refactor'
 end)
--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -20,4 +20,28 @@ configs.setup {
     enable = true,
     enable_autocmd = false,
   },
+  refactor = {
+    highlight_definitions = {
+      enable = true,
+      -- Set to false if you have an `updatetime` of ~100.
+      clear_on_cursor_move = true,
+    },
+    highlight_current_scope = { enable = true },
+    smart_rename = {
+      enable = true,
+      keymaps = {
+        smart_rename = "grr",
+      },
+    },
+    navigation = {
+      enable = true,
+      keymaps = {
+        goto_definition = "gnd",
+        list_definitions = "gnD",
+        list_definitions_toc = "gO",
+        goto_next_usage = "<a-*>",
+        goto_previous_usage = "<a-#>",
+      },
+    },
+  },
 }
