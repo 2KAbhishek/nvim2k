abce22f9f103490922bdf523da0c783973cf4365
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-07-17 13:52:33 +0530

    Remove shortcuts with t

--- plugin/gitsigns.lua
+++ plugin/gitsigns.lua
@@ -68,10 +68,10 @@ require('gitsigns').setup {
         map('n', '<leader>hR', gs.reset_buffer)
         map('n', '<leader>hp', gs.preview_hunk)
         map('n', '<leader>hb', function() gs.blame_line { full = true } end)
-        map('n', '<leader>tb', gs.toggle_current_line_blame)
+        map('n', '<leader>gtb', gs.toggle_current_line_blame)
         map('n', '<leader>hd', gs.diffthis)
         map('n', '<leader>hD', function() gs.diffthis('~') end)
-        map('n', '<leader>td', gs.toggle_deleted)
+        map('n', '<leader>gtd', gs.toggle_deleted)
 
         -- Text object
         map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
