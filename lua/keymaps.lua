017eb7e6e2a8f54151b3c27371bffe9f242f69fb
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-07-18 00:16:06 +0530

    Move general keyboard shortcuts

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -12,6 +12,19 @@ keymap("n", "<C-i>", "<C-i>", opts)
 -- Use jj as escape
 keymap("i", "jj", "<Esc>", opts)
 
+-- Copy to system clipboard
+keymap("v", "<leader>yy", "<cmd>!xclip -f -sel clip<cr>", opts)
+
+-- General
+keymap("n", "<leader>Q", "<cmd>qa!<cr>", opts)
+keymap("n", "<leader>q", "<cmd>bw<cr>", opts)
+keymap("n", "<leader>w", "<cmd>w<cr>", opts)
+keymap("n", "<leader>W", "<cmd>wq<cr>", opts)
+keymap("n", "<leader>r", "<cmd>source ~/.config/nvim/init.vim <cr>", opts)
+keymap("n", "<leader>z", "<cmd>setlocal spell!<cr>", opts)
+keymap("n", "<leader>G", "<cmd>Git<cr>", opts)
+
+
 -- Terminal
 keymap("n", "tt", "<cmd>ToggleTerm<cr>", opts)
 keymap("t", "tt", "<cmd>ToggleTerm<cr>", opts)
@@ -65,3 +78,14 @@ keymap(
     "<cmd>lua require('telescope').extensions.frecency.frecency()<cr>",
     opts
 )
+
+-- LSP
+-- keymap("n", "gd", <cmd>lua vim.lsp.buf.definition()<CR>
+-- keymap("n", "gD", <cmd>lua vim.lsp.buf.declaration()<CR>
+-- keymap("n", "gr", <cmd>lua vim.lsp.buf.references()<CR>
+-- keymap("n", "gi", <cmd>lua vim.lsp.buf.implementation()<CR>
+-- keymap("n", "K", <cmd>lua vim.lsp.buf.hover()<CR>
+-- keymap("n", "<C-k>", <cmd>lua vim.lsp.buf.signature_help()<CR>
+-- keymap("n", "<C-n>", <cmd>lua vim.lsp.diagnostic.goto_prev()<CR>
+-- keymap("n", "<C-p>", <cmd>lua vim.lsp.diagnostic.goto_next()<CR>
+--
