15a39ae3110c91295ecbe4cc32e9b88c59b5c77e
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-07-17 13:37:01 +0530

    Disable null ls formatting

--- plugin/null-ls.lua
+++ plugin/null-ls.lua
@@ -12,10 +12,9 @@ null_ls.setup {
   debug = false,
   sources = {
     formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
-    formatting.black.with { extra_args = { "--fast" } },
-    -- formatting.yapf,
-    formatting.stylua,
-    diagnostics.flake8,
+    -- formatting.black.with { extra_args = { "--fast" } },
+    -- formatting.stylua,
+    -- diagnostics.flake8,
   },
 }
 
