bbcbeb57a390d0eb582d404a8338ce6626f32d1d
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-07-18 12:35:48 +0530

    Add get_input implementation

--- plugin/surround.lua
+++ plugin/surround.lua
@@ -1,61 +1,71 @@
 local status_ok, surround = pcall(require, "nvim-surround")
 if not status_ok then
-  return
+    return
+end
+
+local get_input = function(prompt)
+    local ok, result = pcall(vim.fn.input, { prompt = prompt })
+    if not ok then
+        return nil
+    end
+    return result
 end
 
 surround.setup {
-  keymaps = { -- vim-surround style keymaps
-    insert = "ys",
-    insert_line = "yss",
-    visual = "S",
-    delete = "ds",
-    change = "cs",
-  },
-  delimiters = {
-    pairs = {
-      ["("] = { "( ", " )" },
-      [")"] = { "(", ")" },
-      ["{"] = { "{ ", " }" },
-      ["}"] = { "{", "}" },
-      ["<"] = { "< ", " >" },
-      [">"] = { "<", ">" },
-      ["["] = { "[ ", " ]" },
-      ["]"] = { "[", "]" },
-      -- Define pairs based on function evaluations!
-      ["i"] = function()
-        return {
-          require("nvim-surround.utils").get_input "Enter the left delimiter: ",
-          require("nvim-surround.utils").get_input "Enter the right delimiter: ",
-        }
-      end,
-      ["f"] = function()
-        return {
-          require("nvim-surround.utils").get_input "Enter the function name: " .. "(",
-          ")",
-        }
-      end,
+    keymaps = { -- vim-surround style keymaps
+        insert = "ys",
+        insert_line = "yss",
+        visual = "S",
+        delete = "ds",
+        change = "cs",
     },
-    separators = {
-      ["'"] = { "'", "'" },
-      ['"'] = { '"', '"' },
-      ["`"] = { "`", "`" },
+    delimiters = {
+        pairs = {
+            ["("] = { "( ", " )" },
+            [")"] = { "(", ")" },
+            ["{"] = { "{ ", " }" },
+            ["}"] = { "{", "}" },
+            ["<"] = { "< ", " >" },
+            [">"] = { "<", ">" },
+            ["["] = { "[ ", " ]" },
+            ["]"] = { "[", "]" },
+            -- Define pairs based on function evaluations!
+            ["i"] = function()
+                local left_delimiter = get_input("Enter the left delimiter: ")
+                if left_delimiter then
+                    local right_delimiter = get_input("Enter the right delimiter: ")
+                    if right_delimiter then
+                        return { left_delimiter, right_delimiter }
+                    end
+                end
+            end,
+            ["f"] = function()
+                local result = get_input("Enter the function name: ")
+                if result then
+                    return { result .. "(", ")" }
+                end
+            end,
+        },
+        separators = {
+            ["'"] = { "'", "'" },
+            ['"'] = { '"', '"' },
+            ["`"] = { "`", "`" },
+        },
+        HTML = {
+            ["t"] = "type", -- Change just the tag type
+            ["T"] = "whole", -- Change the whole tag contents
+        },
+        aliases = {
+            ["a"] = ">", -- Single character aliases apply everywhere
+            ["b"] = ")",
+            ["B"] = "}",
+            ["r"] = "]",
+            -- Table aliases only apply for changes/deletions
+            ["q"] = { '"', "'", "`" }, -- Any quote character
+            ["s"] = { ")", "]", "}", ">", "'", '"', "`" }, -- Any surrounding delimiter
+        },
     },
-    HTML = {
-      ["t"] = "type", -- Change just the tag type
-      ["T"] = "whole", -- Change the whole tag contents
+    highlight_motion = { -- Highlight before inserting/changing surrounds
+        duration = 0,
     },
-    aliases = {
-      ["a"] = ">", -- Single character aliases apply everywhere
-      ["b"] = ")",
-      ["B"] = "}",
-      ["r"] = "]",
-      -- Table aliases only apply for changes/deletions
-      ["q"] = { '"', "'", "`" }, -- Any quote character
-      ["s"] = { ")", "]", "}", ">", "'", '"', "`" }, -- Any surrounding delimiter
-    },
-  },
-  highlight_motion = { -- Highlight before inserting/changing surrounds
-    duration = 0,
-  },
 }
-
