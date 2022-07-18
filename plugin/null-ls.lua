6acc03ab17215e60512e6c9bee0bd57e9a7a9c6b
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-07-19 02:13:22 +0530

    Add ruby null-ls configs

--- plugin/null-ls.lua
+++ plugin/null-ls.lua
@@ -12,8 +12,8 @@ null_ls.setup {
     debug = false,
     sources = {
         formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
+        formatting.rubocop,
+        diagnostics.rubocop,
         -- formatting.black.with { extra_args = { "--fast" } },
-        -- formatting.stylua,
-        -- diagnostics.flake8,
     },
 }
