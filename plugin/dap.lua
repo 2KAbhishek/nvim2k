c59e0db9847c06e8e6d857d43a93eb296046c175
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-07-19 02:13:09 +0530

    Add ruby dap configs

--- plugin/dap.lua
+++ plugin/dap.lua
@@ -111,3 +111,22 @@ dap.configurations.python = {
         end;
     },
 }
+
+-- ruby dap configs
+dap.adapters.ruby = {
+    type = 'executable';
+    command = 'bundle';
+    args = { 'exec', 'readapt', 'stdio' };
+}
+
+dap.configurations.ruby = {
+    {
+        type = 'ruby';
+        request = 'launch';
+        name = 'Rails';
+        program = 'bundle';
+        programArgs = { 'exec', 'rails', 's' };
+        useBundler = true;
+    },
+}
+
