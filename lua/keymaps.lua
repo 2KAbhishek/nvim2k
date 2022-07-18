e4a7e4c387d75fa1e3f3f95097beee5294fefa85
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-07-18 22:38:11 +0530

    Update keybindings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -12,8 +12,40 @@ keymap("n", "<C-i>", "<C-i>", opts)
 -- Use jj as escape
 keymap("i", "jj", "<Esc>", opts)
 
--- Copy to system clipboard
-keymap("v", "<leader>yy", "<cmd>!xclip -f -sel clip<cr>", opts)
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Better window navigation
+keymap("n", "<m-h>", "<C-w>h", opts)
+keymap("n", "<m-j>", "<C-w>j", opts)
+keymap("n", "<m-k>", "<C-w>k", opts)
+keymap("n", "<m-l>", "<C-w>l", opts)
+
+-- Tabs --
+keymap("n", "<enter>", ":tabnew %<cr>", opts)
+keymap("n", "<s-enter>", ":tabclose<cr>", opts)
+keymap("n", "<m-\\>", ":tabonly<cr>", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- I hate typing these
+keymap("n", "H", "^", opts)
+keymap("n", "L", "$", opts)
+keymap("v", "H", "^", opts)
+keymap("v", "L", "$", opts)
+keymap("x", "H", "^", opts)
+keymap("x", "L", "$", opts)
+keymap("o", "H", "^", opts)
+keymap("o", "L", "$", opts)
 
 -- General
 keymap("n", "<leader>Q", "<cmd>qa!<cr>", opts)
@@ -24,6 +56,37 @@ keymap("n", "<leader>r", "<cmd>source ~/.config/nvim/init.vim <cr>", opts)
 keymap("n", "<leader>z", "<cmd>setlocal spell!<cr>", opts)
 keymap("n", "<leader>G", "<cmd>Git<cr>", opts)
 keymap("n", "<leader>i", "<cmd>Format<cr>", opts)
+
+-- Naviagate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Move text up and down
+keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- alt binds
+keymap("n", "<m-s>", "<cmd>split<cr>", opts)
+keymap("n", "<m-v>", "<cmd>vsplit<cr>", opts)
+keymap("n", "<m-q>", "<cmd>:q<cr>", opts)
+
+keymap("n", "<esc><esc>", "<cmd>nohlsearch<cr>", opts)
+keymap("n", "gx", [[:silent execute '!$BROWSER ' . shellescape(expand('<cfile>'), 1)<CR>]], opts)
 
 -- Terminal
 keymap("n", "tt", "<cmd>ToggleTerm<cr>", opts)
@@ -45,8 +108,8 @@ keymap("n", "<leader>fs",
 keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", opts)
 keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", opts)
 keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", opts)
-keymap("n", "<leader>fe", "<cmd>Telescope file_browser <cr>", { noremap = true })
-keymap("n", "<leader>f.", "<cmd>Telescope symbols <cr>", { noremap = true })
+keymap("n", "<leader>fe", "<cmd>Telescope file_browser <cr>", opts)
+keymap("n", "<leader>f.", "<cmd>Telescope symbols <cr>", opts)
 
 -- LSP
 keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
