a8f33f0afd54c8882ed8ad540b3bf03dbd34507b
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-07-19 00:26:00 +0530

    Update keybindings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -58,12 +58,12 @@ keymap("n", "<leader>G", "<cmd>Git<cr>", opts)
 keymap("n", "<leader>i", "<cmd>Format<cr>", opts)
 
 -- Naviagate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+-- keymap("n", "<S-l>", ":bnext<CR>", opts)
+-- keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+-- keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+-- keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
@@ -89,34 +89,10 @@ keymap("n", "<esc><esc>", "<cmd>nohlsearch<cr>", opts)
 keymap("n", "gx", [[:silent execute '!$BROWSER ' . shellescape(expand('<cfile>'), 1)<CR>]], opts)
 
 -- Terminal
-keymap("n", "tt", "<cmd>ToggleTerm<cr>", opts)
 keymap("t", "tt", "<cmd>ToggleTerm<cr>", opts)
 
--- Telescope
-keymap("n", "<leader>ff",
-    "<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files prompt_prefix=🔎<cr>",
-    opts
-)
-keymap("n", "<leader>fr",
-    "<cmd>lua require('telescope').extensions.frecency.frecency()<cr>",
-    opts
-)
-keymap("n", "<leader>fs",
-    "<cmd>Telescope current_buffer_fuzzy_find sorting_strategy=ascending<cr>",
-    opts
-)
-keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", opts)
-keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", opts)
-keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", opts)
-keymap("n", "<leader>fe", "<cmd>Telescope file_browser <cr>", opts)
-keymap("n", "<leader>f.", "<cmd>Telescope symbols <cr>", opts)
-
 -- LSP
-keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
 keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
 keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
 keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-keymap("n", "<C-n>", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-keymap("n", "<C-p>", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
