925a984d40fca658bcfa0782d10e4e490643fb0c
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-07-25 03:30:58 +0530

    Update auto commands

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,22 +1,24 @@
+-- Non editable buffer options
 vim.api.nvim_create_autocmd({ "FileType" }, {
     pattern = { "Jaq", "qf", "help", "man", "lspinfo", "spectre_panel", "lir", "DressingSelect", "tsplayground" },
     callback = function()
-        vim.cmd [[
+    vim.cmd [[
       nnoremap <silent> <buffer> q :close<CR>
       set nobuflisted
     ]]
     end,
 })
 
+-- Terminal buffer options
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
     pattern = { "term://*" },
     callback = function()
         vim.cmd "startinsert!"
-        -- TODO: if java = 2
         vim.cmd "set cmdheight=1"
     end,
 })
 
+-- Enable spellcheck on gitcommit and markdown
 vim.api.nvim_create_autocmd({ "FileType" }, {
     pattern = { "gitcommit", "markdown" },
     callback = function()
@@ -25,14 +27,18 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     end,
 })
 
+-- Strip trailing spaces before write
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    pattern = { "*" },
+    callback = function()
+        vim.cmd [[ %s/\s\+$//e ]]
+    end,
+})
+
 vim.cmd([[
     " Have Vim jump to the last position when reopening a file
     if has("autocmd")
         au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\""
     endif
-
-    " Remove trailing whitespace on write
-    if has("autocmd")
-        autocmd BufWritePre * %s/\s\+$//e
-    endif
 ]])
+
