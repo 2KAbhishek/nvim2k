15ddce23589715a011cb47417e8b41c939406799
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-08-23 15:05:09 +0530

    Fix bufferline config

--- plugin/bufferline.lua
+++ plugin/bufferline.lua
@@ -14,7 +14,7 @@ bufferline.setup {
         -- NOTE: this plugin is designed with this icon in mind,
         -- and so changing this is NOT recommended, this is intended
         -- as an escape hatch for people who cannot bear it for whatever reason
-        indicator_icon = '▎',
+        -- indicator_icon = '▎',
         buffer_close_icon = '',
         modified_icon = '●',
         close_icon = '',
@@ -73,33 +73,33 @@ bufferline.setup {
         separator_style = "thin",
         enforce_regular_tabs = false,
         always_show_bufferline = true,
-        sort_by = 'insert_after_current'
-    },
-    groups = {
-        options = {
-            toggle_hidden_on_enter = true -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
-        },
-        items = {
-            {
-                name = "Tests", -- Mandatory
-                highlight = { gui = "underline", guisp = "blue" }, -- Optional
-                priority = 2, -- determines where it will appear relative to other groups (Optional)
-                icon = "", -- Optional
-                matcher = function(buf) -- Mandatory
-                    return buf.name:match('%_test') or buf.name:match('%_spec')
-                end,
+        sort_by = 'insert_after_current',
+        groups = {
+            options = {
+                toggle_hidden_on_enter = true -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
             },
-            {
-                name = "Docs",
-                highlight = { gui = "undercurl", guisp = "green" },
-                auto_close = false, -- whether or not close this group if it doesn't contain the current buffer
-                matcher = function(buf)
-                    return buf.name:match('%.md') or buf.name:match('%.txt')
-                end,
-                separator = { -- Optional
-                    style = require('bufferline.groups').separator.tab
+            items = {
+                {
+                    name = "Tests", -- Mandatory
+                    highlight = { gui = "underline", guisp = "blue" }, -- Optional
+                    priority = 2, -- determines where it will appear relative to other groups (Optional)
+                    icon = "", -- Optional
+                    matcher = function(buf) -- Mandatory
+                        return buf.name:match('%_test') or buf.name:match('%_spec')
+                    end,
                 },
+                {
+                    name = "Docs",
+                    highlight = { gui = "undercurl", guisp = "green" },
+                    auto_close = false, -- whether or not close this group if it doesn't contain the current buffer
+                    matcher = function(buf)
+                        return buf.name:match('%.md') or buf.name:match('%.txt')
+                    end,
+                    separator = { -- Optional
+                        style = require('bufferline.groups').separator.tab
+                    },
+                }
             }
         }
-    }
+    },
 }
