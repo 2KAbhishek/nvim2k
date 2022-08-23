9b06af9e31378219fd3b07cb1bdcafa28a067f5f
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-08-23 14:07:14 +0530

    Remove lspsaga insert mode bindings

--- plugin/lspsaga.lua
+++ plugin/lspsaga.lua
@@ -13,10 +13,10 @@ saga.init_lsp_saga {
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
-vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
+vim.keymap.set('n', '<C-k>', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
-vim.keymap.set('i', 'gs', '<Cmd>Lspsaga signature_help<CR>', opts)
+vim.keymap.set('n', 'gs', '<Cmd>Lspsaga signature_help<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga preview_definition<CR>', opts)
 vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
 vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
