a099d1489b81ddb7c3419bb055b46d571a911f44
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-08-23 14:30:29 +0530

    Update telescope icons

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -8,8 +8,8 @@ local actions = require "telescope.actions"
 telescope.setup {
     defaults = {
 
-        prompt_prefix = " ",
-        selection_caret = " ",
+        prompt_prefix = " ",
+        selection_caret = " ",
         path_display = { "smart" },
 
         mappings = {
