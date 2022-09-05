035d4063d079c55fc9e04d71a76acdfd9f8ba4b2
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-09-06 01:14:25 +0530

    Update twilight context

--- plugin/twilight.lua
+++ plugin/twilight.lua
@@ -10,7 +10,7 @@ twilight.setup {
         color = { "Normal", "#ffffff" },
         inactive = false, -- when true, other windows will be fully dimmed (unless they contain the same buffer)
     },
-    context = 10, -- amount of lines we will try to show around the current line
+    context = 16, -- amount of lines we will try to show around the current line
     treesitter = true, -- use treesitter when available for the filetype
     -- treesitter is used to automatically expand the visible text,
     -- but you can further control the types of nodes that should always be fully expanded
