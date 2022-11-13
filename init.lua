881aa9d1184853e0906832775c14a1aed00a3962
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-11-14 04:39:01 +0530

    Reorder plugin load

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 require("nvim2k.options")
-require("nvim2k.colorscheme")
-require("nvim2k.functions")
-require("nvim2k.keymaps")
 require("nvim2k.plugin-list")
 require("nvim2k.plugins.active")
+require("nvim2k.functions")
+require("nvim2k.keymaps")
+require("nvim2k.colorscheme")
 require("nvim2k.autocmd")
 
