e9f22a83b19f2c7a64704dc18d8b450df2d37bb9
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-12-19 02:17:33 +0530

    Add cmp plugin

--- lua/nvim2k/plugin-list.lua
+++ lua/nvim2k/plugin-list.lua
@@ -98,6 +98,7 @@ return require('packer').startup(function(use)
     -- Completion
     use {
         'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-nvim-lua',
         'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-path',
         'hrsh7th/cmp-cmdline',
