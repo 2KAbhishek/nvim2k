48abbdd4e928c0431f6e4dae87d5d4f30889b7d4
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-12-19 02:17:23 +0530

    Update colorscheme configs

--- lua/nvim2k/colorscheme.lua
+++ lua/nvim2k/colorscheme.lua
@@ -1,13 +1,9 @@
-vim.cmd([[
-    let g:onedark_color_overrides = {
-    \ "comment_grey": {"gui": "#69747C","cterm": "245", "cterm16": "8"},
-    \ "gutter_fg_grey": { "gui": "#69747C", "cterm": "245", "cterm16": "8"}
-    \}
-    if !exists('$TMUX')
-        let g:onedark_terminal_italics = 1
-    endif
-    colorscheme onedark
+function VimColor(color)
+    color = color or "onedark"
+    vim.cmd.colorscheme(color)
 
-    " Always use terminal background
-    highlight Normal guibg=none
-]])
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+VimColor()
