9fab9650c3a07443fa2ac7df377d30d75b1d979c
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-12-26 16:16:38 +0530

    rft: use command names for dap binds

--- lua/nvim2k/plugins/which-key.lua
+++ lua/nvim2k/plugins/which-key.lua
@@ -106,15 +106,16 @@ local mappings = {
 
     d = {
         name = "Debug",
-        b = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Breakpoint" },
-        c = { "<cmd>lua require'dap'.continue()<cr>", "Continue" },
-        i = { "<cmd>lua require'dap'.step_into()<cr>", "Into" },
-        o = { "<cmd>lua require'dap'.step_over()<cr>", "Over" },
-        O = { "<cmd>lua require'dap'.step_out()<cr>", "Out" },
-        r = { "<cmd>lua require'dap'.repl.toggle()<cr>", "Repl" },
         l = { "<cmd>lua require'dap'.run_last()<cr>", "Last" },
         u = { "<cmd>lua require'dapui'.toggle()<cr>", "UI" },
-        x = { "<cmd>lua require'dap'.terminate()<cr>", "Exit" },
+        b = { "<cmd>DapToggleBreakpoint<cr>", "Breakpoint" },
+        c = { "<cmd>DapContinue<cr>", "Continue" },
+        i = { "<cmd>DapStepInto<cr>", "Into" },
+        o = { "<cmd>DapStepOver<cr>", "Over" },
+        O = { "<cmd>DapStepOut<cr>", "Out" },
+        r = { "<cmd>DapToggleRepl<cr>", "Repl" },
+        R = { "<cmd>DapRestartFrame<cr>", "Repl" },
+        x = { "<cmd>DapTerminate<cr>", "Exit" },
     },
 
     f = {
