7e56e68ee65484c2d231294865a58b55022969bf
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-12-26 15:24:12 +0530

    feat: load cmp plugins on insert

--- lua/nvim2k/plugin-list.lua
+++ lua/nvim2k/plugin-list.lua
@@ -38,15 +38,20 @@ local plugins = {
     "glepnir/lspsaga.nvim",
 
     -- Completion
-    'hrsh7th/nvim-cmp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-cmdline',
-    'saadparwaiz1/cmp_luasnip',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-nvim-lua',
-    'L3MON4D3/LuaSnip',
-    'rafamadriz/friendly-snippets',
+    {
+        'hrsh7th/nvim-cmp',
+        event = "InsertEnter",
+        dependencies = {
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lua',
+            'L3MON4D3/LuaSnip',
+            'rafamadriz/friendly-snippets',
+        },
+    },
 
     -- Telescope
     'nvim-lua/plenary.nvim',
