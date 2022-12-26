c4052ec3e963ab38702ec0a1ce4b3a5cfaeb35f0
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-12-26 12:09:59 +0530

    feat: add lsp-zero configs

---
+++ lua/nvim2k/plugins/lsp-zero.lua
@@ -0,0 +1,17 @@
+vim.opt.signcolumn = 'yes' -- Reserve space for diagnostic icons
+
+local status_ok, lsp = pcall(require, 'lsp-zero')
+if not status_ok then
+    return
+end
+lsp.preset('recommended')
+
+lsp.ensure_installed({
+  'tsserver',
+  'eslint',
+  'sumneko_lua',
+})
+
+lsp.nvim_workspace()
+
+lsp.setup()
