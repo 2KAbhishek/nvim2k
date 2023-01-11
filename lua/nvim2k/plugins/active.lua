1b629f07a00bffdc105b2cd88f676f2539972a6b
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-01-11 14:54:14 +0530

    add: oil configs

--- lua/nvim2k/plugins/active.lua
+++ lua/nvim2k/plugins/active.lua
@@ -23,6 +23,7 @@ require 'nvim2k.plugins.toggleterm'
 require 'nvim2k.plugins.treesitter-context'
 require 'nvim2k.plugins.treesitter'
 require 'nvim2k.plugins.telekasten'
+require 'nvim2k.plugins.oil'
 
 -- LSP Stuff
 require 'nvim2k.plugins.lsp-zero'
