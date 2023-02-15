84756b803f6087fdf0e0792d3ac2673263408d82
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-02-16 05:20:49 +0530

    feat: update lspsage to latest configs

--- lua/nvim2k/plugins/lspsaga.lua
+++ lua/nvim2k/plugins/lspsaga.lua
@@ -3,11 +3,25 @@ if not status_ok then
     return
 end
 
-saga.init_lsp_saga {
-    border_style = "rounded",
-    server_filetype_map = {
-        typescript = 'typescript'
-    }
+saga.setup {
+    ui = {
+        -- Currently, only the round theme exists
+        theme = "round",
+        -- This option only works in Neovim 0.9
+        title = true,
+        -- Border type can be single, double, rounded, solid, shadow.
+        border = "rounded",
+        winblend = 0,
+        expand = "",
+        collapse = "",
+        preview = " ",
+        code_action = "💡",
+        diagnostic = "🐞",
+        incoming = " ",
+        outgoing = " ",
+        hover = ' ',
+        kind = {},
+    },
 }
 
 
@@ -21,4 +35,3 @@ vim.keymap.set('n', 'gp', '<Cmd>Lspsaga preview_definition<CR>', opts)
 vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
 vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
 vim.keymap.set("v", "<leader>ca", "<cmd><C-U>Lspsaga range_code_action<CR>", opts)
-
