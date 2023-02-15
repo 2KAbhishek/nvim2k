a3cde87321da107386ef182a2a76155faaddf245
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-02-16 04:42:27 +0530

    feat: use one darker

--- lua/nvim2k/plugins/onedark.lua
+++ lua/nvim2k/plugins/onedark.lua
@@ -5,7 +5,7 @@ end
 
 onedark.setup {
     -- Main options --
-    style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+    style = 'darker', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
     transparent = true, -- Show/hide background
     term_colors = false, -- Change terminal color as per the selected theme style
     ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
