0df282289203dce132c06da0bf17c5df95bcbcaa
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-02-16 04:33:11 +0530

    refactor(code): cleanup colorscheme files

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@ require("nvim2k.options")
 require("nvim2k.keymaps")
 require("nvim2k.plugin-list")
 require("nvim2k.functions")
-require("nvim2k.colorscheme")
 require("nvim2k.autocmd")
 require("nvim2k.plugins.active")
 
--- lua/nvim2k/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-function VimColor(color)
-    color = color or "onedark"
-    vim.cmd.colorscheme(color)
-
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
-VimColor()
