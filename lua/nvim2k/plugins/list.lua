d6de7e92725ed8a9d20671477c693c74e42478e2
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-02-16 06:54:29 +0530

    refactor: update plugin list location

--- lua/nvim2k/plugins/list.lua
+++ lua/nvim2k/plugins/list.lua
@@ -1,17 +1,4 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        'git',
-        'clone',
-        '--filter=blob:none',
-        '--single-branch',
-        'https://github.com/folke/lazy.nvim.git',
-        lazypath,
-    })
-end
-vim.opt.runtimepath:prepend(lazypath)
-
-local plugins = {
+return {
     -- Appearance
     'navarasu/onedark.nvim',
     'nvim-lualine/lualine.nvim',
@@ -135,5 +122,3 @@ local plugins = {
     -- Language specific
     { 'tpope/vim-rails', ft = "ruby" }
 }
-
-require('lazy').setup(plugins)
