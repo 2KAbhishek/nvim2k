726cc2461ddd6ded0b5bf4b7262b08b5ca500e62
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-02-24 05:37:08 +0530

    feat: update lualine icon

--- lua/nvim2k/plugins/lualine.lua
+++ lua/nvim2k/plugins/lualine.lua
@@ -90,7 +90,7 @@ ins_left {
 ins_left {
     -- mode component
     function()
-        return ' '
+        return ' '
     end,
     color = function()
         -- auto change color according to neovims mode
