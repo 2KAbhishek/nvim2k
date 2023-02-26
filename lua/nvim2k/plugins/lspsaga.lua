75384211387b60439b1fbc3d77cedeb39769fc5f
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-02-27 04:11:33 +0530

    feat: disable conflicting keybinds

--- lua/nvim2k/plugins/lspsaga.lua
+++ lua/nvim2k/plugins/lspsaga.lua
@@ -25,8 +25,8 @@ saga.setup({
 })
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
-vim.keymap.set('n', '<C-k>', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
+vim.keymap.set('n', '<leader>lj', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+vim.keymap.set('n', '<leader>lk', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('n', 'gs', '<Cmd>Lspsaga signature_help<CR>', opts)
