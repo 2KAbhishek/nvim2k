0b453615b1a51c8c26c50d92d22a61c16903a7d2
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-03-29 01:34:12 +0530

    feat: renabled disabled plugins

--- lua/nvim2k/plugins/lazy.lua
+++ lua/nvim2k/plugins/lazy.lua
@@ -137,14 +137,14 @@ lazy.setup({
             paths = {}, -- add any custom paths here that you want to includes in the rtp
             ---@type string[] list any plugins you want to disable here
             disabled_plugins = {
-                'gzip',
-                'matchit',
-                'matchparen',
-                'netrwPlugin',
-                'tarPlugin',
-                'tohtml',
-                'tutor',
-                'zipPlugin',
+                -- 'gzip',
+                -- 'matchit',
+                -- 'matchparen',
+                -- 'netrwPlugin',
+                -- 'tarPlugin',
+                -- 'tohtml',
+                -- 'tutor',
+                -- 'zipPlugin',
             },
         },
     },
