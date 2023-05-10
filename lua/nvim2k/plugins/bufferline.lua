f4766060678d214aeb266e586c30674a2f00c471
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-05-10 23:25:48 +0530

    feat: remove deprecated icon

--- lua/nvim2k/plugins/bufferline.lua
+++ lua/nvim2k/plugins/bufferline.lua
@@ -64,7 +64,6 @@ bufferline.setup({
         color_icons = true, -- whether or not to add the filetype icon highlights
         show_buffer_icons = true, -- disable filetype icons for buffers
         show_buffer_close_icons = true,
-        show_buffer_default_icon = true, -- whether or not an unrecognised filetype should show a default icon
         show_close_icon = true,
         show_tab_indicators = true,
         persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
