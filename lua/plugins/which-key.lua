398e52fd5c3fc98110036f03b7f827c11b13cb30
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-09-06 03:40:45 +0530

    docs: update comments

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,22 +5,22 @@ end
 
 local setup = {
     plugins = {
-        marks = true, -- shows a list of your marks on ' and `
-        registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+        marks = true,         -- shows a list of your marks on ' and `
+        registers = true,     -- shows your registers on " in NORMAL or <C-r> in INSERT mode
         spelling = {
-            enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+            enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
             suggestions = 20, -- how many suggestions should be shown in the list?
         },
         -- the presets plugin, adds help for a bunch of default keybindings in Neovim
         -- No actual key bindings are created
         presets = {
-            operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-            motions = true, -- adds help for motions
+            operators = true,    -- adds help for operators like d, y, ... and registers them for motion / text object completion
+            motions = true,      -- adds help for motions
             text_objects = true, -- help for text objects triggered after entering an operator
-            windows = true, -- default bindings on <c-w>
-            nav = true, -- misc bindings to work with windows
-            z = true, -- bindings for folds, spelling and others prefixed with z
-            g = true, -- bindings for prefixed with g
+            windows = true,      -- default bindings on <c-w>
+            nav = true,          -- misc bindings to work with windows
+            z = true,            -- bindings for folds, spelling and others prefixed with z
+            g = true,            -- bindings for prefixed with g
         },
     },
     -- add operators that will trigger motion and text object completion
@@ -41,26 +41,26 @@ local setup = {
     },
     popup_mappings = {
         scroll_down = '<c-d>', -- binding to scroll down inside the popup
-        scroll_up = '<c-u>', -- binding to scroll up inside the popup
+        scroll_up = '<c-u>',   -- binding to scroll up inside the popup
     },
     window = {
-        border = 'none', -- none, single, double, shadow
-        position = 'bottom', -- bottom, top
-        margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+        border = 'none',          -- none, single, double, shadow
+        position = 'bottom',      -- bottom, top
+        margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
         padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
         winblend = 15,
     },
     layout = {
         height = { min = 4, max = 25 }, -- min and max height of the columns
         width = { min = 20, max = 50 }, -- min and max width of the columns
-        spacing = 3, -- spacing between columns
-        align = 'center', -- align columns left, center or right
+        spacing = 3,                    -- spacing between columns
+        align = 'center',               -- align columns left, center or right
     },
-    ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
-    hidden = { '<silent>', '<cmd>', '<Cmd>', '<CR>', '^:', '^ ', '^call ', '^lua ' }, -- hide mapping boilerplate
-    show_help = true, -- show a help message in the command line for using WhichKey
-    show_keys = true, -- show the currently pressed key and its label as a message in the command line
-    triggers = 'auto', -- automatically setup triggers
+    ignore_missing = true,              -- enable this to hide mappings for which you didn't specify a label
+    hidden = { '<silent>', '<cmd>', '<Cmd>', '<CR>', '^:', '^ ', '^call ', '^lua ' },
+    show_help = true,                   -- show a help message in the command line for using WhichKey
+    show_keys = true,                   -- show the currently pressed key and its label as a message in the command line
+    triggers = 'auto',                  -- automatically setup triggers
     -- triggers = {"<leader>"} -- or specifiy a list manually
     -- list of triggers, where WhichKey should not wait for timeoutlen and show immediately
     triggers_nowait = {
@@ -120,12 +120,12 @@ for key, name in pairs({ n = 'Next', l = 'Last' }) do
 end
 
 local opts = {
-    mode = 'n', -- NORMAL mode
+    mode = 'n',     -- NORMAL mode
     prefix = '<leader>',
-    buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-    silent = true, -- use `silent` when creating keymaps
+    buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
+    silent = true,  -- use `silent` when creating keymaps
     noremap = true, -- use `noremap` when creating keymaps
-    nowait = true, -- use `nowait` when creating keymaps
+    nowait = true,  -- use `nowait` when creating keymaps
 }
 
 local mappings = {
@@ -175,9 +175,9 @@ local mappings = {
         o = { '<cmd>DapStepOver<cr>', 'Over' },
         O = { '<cmd>DapStepOut<cr>', 'Out' },
         r = { '<cmd>DapToggleRepl<cr>', 'Repl' },
-        s = {'<cmd>lua require("dbee").store("csv", "buffer", { extra_arg = 0 })<cr>', 'DB to CSV'},
-        S = {'<cmd>lua require("dbee").store("json", "buffer", { extra_arg = 0 })<cr>', 'DB to JSON'},
-        t = {'<cmd>lua require("dbee").store("table", "buffer", { extra_arg = 0 })<cr>', 'DB to Table'},
+        s = { '<cmd>lua require("dbee").store("csv", "buffer", { extra_arg = 0 })<cr>', 'DB to CSV' },
+        S = { '<cmd>lua require("dbee").store("json", "buffer", { extra_arg = 0 })<cr>', 'DB to JSON' },
+        t = { '<cmd>lua require("dbee").store("table", "buffer", { extra_arg = 0 })<cr>', 'DB to Table' },
         R = { '<cmd>DapRestartFrame<cr>', 'Restart Frame' },
         x = { '<cmd>DapTerminate<cr>', 'Exit' },
     },
@@ -471,12 +471,12 @@ local mappings = {
 }
 
 local vopts = {
-    mode = 'v', -- VISUAL mode
+    mode = 'v',     -- VISUAL mode
     prefix = '<leader>',
-    buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-    silent = true, -- use `silent` when creating keymaps
+    buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
+    silent = true,  -- use `silent` when creating keymaps
     noremap = true, -- use `noremap` when creating keymaps
-    nowait = true, -- use `nowait` when creating keymaps
+    nowait = true,  -- use `nowait` when creating keymaps
 }
 
 local vmappings = {
