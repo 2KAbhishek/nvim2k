0a80fd7aec1b366ad24206975a87c7907d4ea556
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-09-11 12:54:50 +0530

    feat: reorder cmp sources

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -92,10 +92,10 @@ cmp.setup({
     sources = {
         { name = 'copilot', group_index = 2 },
         { name = 'nvim_lsp', group_index = 2 },
-        { name = 'nvim_lua', group_index = 2 },
-        { name = 'luasnip', group_index = 2 },
         { name = 'buffer', group_index = 2 },
         { name = 'path', group_index = 2 },
+        { name = 'nvim_lua', group_index = 2 },
+        { name = 'luasnip', group_index = 2 },
     },
     sorting = {
         priority_weight = 2,
