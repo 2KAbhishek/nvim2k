8f239329941f329f31cb898500ba3cfbc0070481
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-09-22 17:59:06 +0530

    refactor: move easypick to disabled

--- lua/plugins/gitlinker.lua
+++ lua/plugins/gitlinker.lua
@@ -30,5 +30,4 @@ gitlinker.setup({
   mappings = "<leader>gy"
 })
 
--- vim.api.nvim_set_keymap('n', '<leader>gy', '<cmd>lua require"gitlinker".get_buf_range_url()<cr>', {silent = true})
 -- vim.api.nvim_set_keymap('v', '<leader>gy', '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>', {})
