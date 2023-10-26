0fd750caff3ac22c6528989a30e207b109e95d2b
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-10-26 18:12:38 +0530

    fix: lua lsp whitespace formatting

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -14,6 +14,15 @@ require('mason-lspconfig').setup({
         lsp_zero.default_setup,
         lua_ls = function()
             local lua_opts = lsp_zero.nvim_lua_ls()
+            local custom_options = {
+                enable = true,
+                defaultConfig = {
+                    align_continuous_assign_statement = false,
+                    align_continuous_rect_table_field = false,
+                    align_array_table = false
+                }
+            }
+            lua_opts.settings.Lua.format = custom_options
             require('lspconfig').lua_ls.setup(lua_opts)
         end,
     }
