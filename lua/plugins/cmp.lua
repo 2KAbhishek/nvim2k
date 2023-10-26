5bbdef88650ac0c578446bc9ea17646162b5cc6e
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-10-26 18:12:53 +0530

    feat: add cmp doc scroll

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -28,6 +28,8 @@ cmp.setup({
         ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-1), { 'i', 'c' }),
         ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(1), { 'i', 'c' }),
         ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
         ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
         ['<C-c>'] = cmp.mapping({
             i = cmp.mapping.abort(),
