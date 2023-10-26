caf0a124d6dacae7523ccc26fa257b439806773a
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-10-26 14:47:43 +0530

    feat: update comment keys

--- lua/installed.lua
+++ lua/installed.lua
@@ -285,7 +285,10 @@ return {
         config = function()
             require('plugins.comment')
         end,
-        keys = { 'gcc', 'gbc' },
+        keys = {
+            { 'gcc', mode = { 'n', }, function() require('Comment').toggle() end, desc = "Comment" },
+            { 'gc', mode = { 'v' }, function() require('Comment').toggle() end, desc = "Comment" },
+        },
     },
     {
         'kylechui/nvim-surround',
@@ -325,7 +328,7 @@ return {
             { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
             { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
             { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-            { "R", mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
             { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
         },
     },
