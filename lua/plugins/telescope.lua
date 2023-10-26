f8562663631f6c2bdfcc51eb8df66528514c5e4a
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-10-27 02:45:02 +0530

    fix: cleanup unused plugin configs

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -134,35 +134,11 @@ telescope.setup({
         oldfiles = { mappings = multi_open_mappings },
     },
     extensions = {
-        file_browser = {
-            theme = 'ivy',
-            -- disables netrw and use telescope-file-browser in its place
-            hijack_netrw = true,
-            mappings = {
-                ['i'] = {
-                    -- your custom insert mode mappings
-                },
-                ['n'] = {
-                    -- your custom normal mode mappings
-                },
-            },
-        },
         fzf = {
-            fuzzy = true, -- false will only do exact matching
-            override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true, -- override the file sorter
-            case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
-        },
-        frecency = {
-            -- db_root = "home/my_username/path/to/db_root",
-            show_scores = false,
-            show_unindexed = true,
-            ignore_patterns = { '*.git/*', '*/tmp/*' },
-            disable_devicons = false,
-            -- workspaces = {
-            --   ["Projects"] = "/home/abhishek/Projects",
-            -- }
-        },
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = 'smart_case',
         },
         undo = {
             use_delta = true,
