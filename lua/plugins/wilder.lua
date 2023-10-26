7e6c79cafe12177100192e74f8cef3bc9a243447
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-10-27 00:50:28 +0530

    feat: use fd in wilder

--- lua/plugins/wilder.lua
+++ lua/plugins/wilder.lua
@@ -8,13 +8,8 @@ wilder.setup({ modes = { ':', '/', '?' } })
 wilder.set_option('pipeline', {
     wilder.branch(
         wilder.python_file_finder_pipeline({
-            -- to use ripgrep : {'rg', '--files'}
-            -- to use fd      : {'fd', '-tf'}
             file_command = { 'rg', '--files', '--hidden' },
-            -- to use fd      : {'fd', '-td'}
-            dir_command = { 'find', '.', '-type', 'd', '-printf', '%P\n' },
-            -- use {'cpsm_filter'} for performance, requires cpsm vim plugin
-            -- found at https://github.com/nixprime/cpsm
+            dir_command = { 'fd', '-td'},
             filters = { 'fuzzy_filter', 'difflib_sorter' },
         }),
         wilder.cmdline_pipeline({
