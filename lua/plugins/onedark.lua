1c48942b53adc0164b6ebd7ffd6fcbddbe6d7a15
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-17 09:32:15 +0530

    feat: add transparent background!!

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -52,3 +52,9 @@ onedark.setup({
 })
 
 onedark.load()
+
+-- transparent background
+vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+vim.api.nvim_set_hl(0, "FloatBorder", {bg = "none"})
+vim.api.nvim_set_hl(0, "Pmenu", {bg = "none"})
