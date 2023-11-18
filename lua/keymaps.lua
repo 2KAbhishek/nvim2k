646e196b60ee6516994d3b3322b14fd49a4b0753
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-18 09:08:14 +0530

    feat: cleanup unused keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,5 @@
+-- This file only tweaks the default keymaps for setting new ones use which-key
+
 local function map(mode, lhs, rhs, opts)
     opts = opts or {}
     opts.silent = opts.silent ~= false
@@ -10,51 +12,23 @@ map('n', '<Space>', '', opts)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-map('n', '<C-f>', '<cmd>Telescope find_files<cr>', opts)
-map('n', '<C-g>', '<cmd>LazygitToggle<cr>', opts)
+-- Modes
+-- normal_mode = "n", insert_mode = "i", visual_mode = "v",
+-- visual_block_mode = "x", term_mode = "t", command_mode = "c",
 
 -- Use jj as escape
 map('i', 'jj', '<Esc>', opts)
+map('t', 'JJ', '<C-\\><C-n>', opts)
 
--- Modes
---   normal_mode = "n",
---   insert_mode = "i",
---   visual_mode = "v",
---   visual_block_mode = "x",
---   term_mode = "t",
---   command_mode = "c",
-
--- Delete a word backwards
--- map('n', 'dw', 'vb"_d', opts)
-
--- I hate typing these
--- map("n", "H", "^", opts)
--- map("n", "L", "$", opts)
-map('v', 'H', '^', opts)
-map('v', 'L', '$', opts)
-map('x', 'H', '^', opts)
-map('x', 'L', '$', opts)
-map('o', 'H', '^', opts)
-map('o', 'L', '$', opts)
-
--- Paste
-map('v', 'p', '"_dP', opts)
-map('x', '<leader>pp', '"_dP', opts)
+-- Visual overwrite paste
+map({ 'v', 'x' }, 'p', '"_dP', opts)
 
 -- Do not copy on x
-map('n', 'x', '"_x', opts)
+map({ 'n', 'v', 'x' }, 'x', '"_x', opts)
 
 -- Increment/decrement
-map('n', '+', '<C-a>', opts)
-map('n', '-', '<C-x>', opts)
-
--- alt binds
-map('n', '<m-s>', '<cmd>split<cr>', opts)
-map('n', '<m-v>', '<cmd>vsplit<cr>', opts)
-map('n', '<m-q>', '<cmd>:q<cr>', opts)
-
--- open link under cursor
-map('n', 'gx', [[:silent execute '!$BROWSER ' . shellescape(expand('<cfile>'), 1)<CR>]], opts)
+map({ 'n', 'v', 'x' }, '=', '<C-a>', opts)
+map({ 'n', 'v', 'x' }, '-', '<C-x>', opts)
 
 -- Center Cursors
 map('n', 'J', 'mzJ`z', opts)
@@ -63,28 +37,21 @@ map('n', '<C-u>', '<C-u>zz', opts)
 map('n', 'n', 'nzzzv', opts)
 map('n', 'N', 'Nzzzv', opts)
 
--- better up/down
+-- Better up/down
 map('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 map('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
+-- Alternate buffer
 map('n', '<leader><tab>', '<cmd>e #<cr>', { desc = 'Switch to Other Buffer' })
-map('n', '<leader>`', '<cmd>e #<cr>', { desc = 'Switch to Other Buffer' })
 
 -- Move text up and down
-map('v', '<A-j>', ':m .+1<CR>==', opts)
-map('v', '<A-k>', ':m .-2<CR>==', opts)
-map('x', 'J', ":move '>+1<CR>gv-gv", opts)
-map('x', 'K', ":move '<-2<CR>gv-gv", opts)
-map('x', '<A-j>', ":move '>+1<CR>gv-gv", opts)
-map('x', '<A-k>', ":move '<-2<CR>gv-gv", opts)
+map({ 'v', 'x', }, 'J', ":move '>+1<CR>gv-gv", opts)
+map({ 'v', 'x', }, 'K', ":move '<-2<CR>gv-gv", opts)
 
 -- Clear search, diff update and redraw
 map({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and clear hlsearch' })
 
--- Search word under cursor
-map({ 'n', 'x' }, 'gw', '*N', { desc = 'Search word under cursor' })
-
--- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+-- Consistent n/N search navigation
 map('n', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next search result' })
 map('x', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next search result' })
 map('o', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next search result' })
@@ -92,12 +59,6 @@ map('n', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev search result
 map('x', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev search result' })
 map('o', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev search result' })
 
--- Add undo break-points
-map('i', ',', ',<c-g>u')
-map('i', '.', '.<c-g>u')
-map('i', ';', ';<c-g>u')
-
--- better indenting
+-- Better indenting
 map('v', '<', '<gv')
 map('v', '>', '>gv')
-
