382c79849fa4914ab933891f68a36ec01b386f3b
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-22 12:12:32 +0530

    fix: spell check conflict

--- lua/plugins/lang/textobjects.lua
+++ lua/plugins/lang/textobjects.lua
@@ -62,7 +62,7 @@ local textobjects = {
 
             -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
             -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-            ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+            ["]S"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
             ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
         },
         goto_next_end = {
