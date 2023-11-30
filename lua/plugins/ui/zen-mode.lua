88f83cfe4cc3e47ee7d71183302fb244d30bbfac
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-30 19:47:50 +0530

    feat: hide everything in zenmode

--- lua/plugins/ui/zen-mode.lua
+++ lua/plugins/ui/zen-mode.lua
@@ -11,17 +11,17 @@ zen.setup({
         -- * a percentage of the width / height of the editor when <= 1
         -- * a function that returns the width or the height
         width = 180, -- width of the Zen window
-        height = 1, -- height of the Zen window
+        height = 1,  -- height of the Zen window
         -- by default, no options are changed for the Zen window
         -- uncomment any of the options below, or add other vim.wo options you want to apply
         options = {
-            -- signcolumn = "no", -- disable signcolumn
-            -- number = false, -- disable number column
-            -- relativenumber = false, -- disable relative numbers
-            -- cursorline = false, -- disable cursorline
-            -- cursorcolumn = false, -- disable cursor column
-            -- foldcolumn = "0", -- disable fold column
-            -- list = false, -- disable whitespace characters
+            signcolumn = "no",      -- disable signcolumn
+            number = false,         -- disable number column
+            relativenumber = false, -- disable relative numbers
+            cursorline = false,     -- disable cursorline
+            cursorcolumn = false,   -- disable cursor column
+            foldcolumn = "0",       -- disable fold column
+            list = false,           -- disable whitespace characters
         },
     },
     plugins = {
@@ -29,12 +29,12 @@ zen.setup({
         -- comment the lines to not apply the options
         options = {
             enabled = true,
-            ruler = false, -- disables the ruler text in the cmd line area
-            showcmd = false, -- disables the command in the last line of the screen
+            ruler = true,               -- disables the ruler text in the cmd line area
+            showcmd = true,             -- disables the command in the last line of the screen
         },
-        twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
-        gitsigns = { enabled = false }, -- disables git signs
-        tmux = { enabled = true }, -- disables the tmux statusline
+        twilight = { enabled = true },  -- enable to start Twilight when zen mode opens
+        gitsigns = { enabled = true }, -- disables git signs
+        tmux = { enabled = true },      -- disables the tmux statusline
         -- this will change the font size on kitty when in zen mode
         -- to make this work, you need to set the following kitty options:
         -- - allow_remote_control socket-only
@@ -45,7 +45,11 @@ zen.setup({
         },
     },
     -- callback where you can add custom code when the Zen window opens
-    on_open = function(win) end,
+    on_open = function(win)
+        vim.o.laststatus = 0
+    end,
     -- callback where you can add custom code when the Zen window closes
-    on_close = function() end,
+    on_close = function()
+        vim.o.laststatus = 2
+    end,
 })
