548ec1b28d645513f7fb2c2be3b17275cd911472
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-02-05 15:08:43 +0530

    feat: remove obsolete todo function

--- lua/core/functions.lua
+++ lua/core/functions.lua
@@ -24,25 +24,3 @@ vim.api.nvim_create_user_command('CApath', function()
     vim.notify('Copied "' .. path .. '" to the clipboard!')
 end, {})
 
--- Find and list files with unchecked markdown checkboxes in $NOTES_DIR
-vim.api.nvim_create_user_command('Todos', function()
-    local notes_dir = vim.env.NOTES_DIR
-    local files = vim.fn.systemlist('rg -le "\\[ \\]" --sort created ' .. notes_dir)
-    local result = {}
-    for _, file in ipairs(files) do
-        if vim.fn.filereadable(file) == 1 then
-            table.insert(result, file)
-        end
-    end
-
-    if #result > 0 then
-        vim.ui.select(result, { prompt = 'Pending TODOs:' }, function(item, _)
-            if not item then
-                return
-            end
-            if item then
-                vim.cmd('edit ' .. item)
-            end
-        end)
-    end
-end, {})
