7237dc91f1b134319db9a9fd2ce9a687d3d7d805
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-02-25 07:48:30 +0530

    feat: revisit lang

--- lua/plugins/lang/treesitter.lua
+++ lua/plugins/lang/treesitter.lua
@@ -38,20 +38,18 @@ configs.setup({
     ignore_install = {},
     auto_install = true,
 
-    autopairs = {
-        enable = true,
-    },
-
-    endwise = {
-        enable = true,
-    },
+    textobjects = textobjects,
+    autopairs = { enable = true },
+    endwise = { enable = true },
+    autotag = { enable = true },
+    matchup = { enable = true },
+    indent = { enable = true },
 
     highlight = {
         enable = true,
         disable = {},
         additional_vim_regex_highlighting = false,
     },
-    indent = { enable = true },
 
     context_commentstring = {
         enable = true,
@@ -65,6 +63,22 @@ configs.setup({
             node_incremental = '<c-space>',
             scope_incremental = false,
             node_decremental = '<bs>',
+        },
+    },
+
+    textsubjects = {
+        enable = true,
+        prev_selection = ',',
+        keymaps = {
+            ['.'] = { 'textsubjects-smart', desc = 'Select the current text subject' },
+            ['a;'] = {
+                'textsubjects-container-outer',
+                desc = 'Select outer container (class, function, etc.)',
+            },
+            ['i;'] = {
+                'textsubjects-container-inner',
+                desc = 'Select inside containers (classes, functions, etc.)',
+            },
         },
     },
 
@@ -91,23 +105,4 @@ configs.setup({
             },
         },
     },
-
-    autotag = {
-        enable = true,
-    },
-
-    matchup = {
-        enable = true,
-    },
-
-    textsubjects = {
-        enable = true,
-        prev_selection = ',',
-        keymaps = {
-            ['.'] = { 'textsubjects-smart', desc = 'Select the current text subject' },
-            ['a;'] = { 'textsubjects-container-outer', desc = 'Select outer container (class, function, etc.)' },
-            ['i;'] = { 'textsubjects-container-inner', desc = 'Select inside containers (classes, functions, etc.)' },
-        },
-    },
-    textobjects = textobjects,
 })
