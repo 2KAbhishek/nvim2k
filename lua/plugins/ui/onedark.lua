5155fa831f65afcb9cd47e09d1b59b2e279ae000
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-02-25 07:24:17 +0530

    feat: darker diagnostics

--- lua/plugins/ui/onedark.lua
+++ lua/plugins/ui/onedark.lua
@@ -4,20 +4,16 @@ if not status_ok then
 end
 
 onedark.setup({
-    -- Main options --
-    style = 'deep', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-    transparent = true, -- Show/hide background
-    term_colors = false, -- Change terminal color as per the selected theme style
-    ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-    cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+    style = 'deep',
+    transparent = true,
+    term_colors = false,
+    ending_tildes = false,
+    cmp_itemkind_reverse = false,
 
-    -- toggle theme style ---
-    toggle_style_key = '<leader>ct', -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-    toggle_style_list = { 'dark', 'darker', 'cool', 'warm', 'warmer', 'deep' }, -- List of styles to toggle between
+    toggle_style_key = '<leader>ct',
+    toggle_style_list = { 'dark', 'darker', 'cool', 'warm', 'warmer', 'deep', 'light' },
 
-    -- Change code style ---
-    -- Options are italic, bold, underline, none
-    -- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
+    -- Options are italic, bold, underline, none and combos 'italic,bold'
     code_style = {
         comments = 'italic',
         keywords = 'none',
@@ -26,35 +22,13 @@ onedark.setup({
         variables = 'none',
     },
 
-    -- Lualine options --
-    lualine = {
-        transparent = true, -- lualine center bar transparency
-    },
-
-    -- Custom Highlights --
-    colors = {
-        -- bright_orange = "#ff8800", -- define a new color
-        -- green = '#00ffaa', -- redefine an existing color
-    }, -- Override default colors
-    highlights = {
-        -- ["@keyword"] = { fg = '$green' },
-        -- ["@string"] = { fg = '$bright_orange', bg = '#00ff00', fmt = 'bold' },
-        -- ["@function"] = { fg = '#0000ff', sp = '$cyan', fmt = 'underline,italic' },
-        -- ["@function.builtin"] = { fg = '#0059ff' }
-    }, -- Override highlight groups
-
-    -- Plugins Config --
-    diagnostics = {
-        darker = false, -- darker colors for diagnostic
-        undercurl = true, -- use undercurl instead of underline for diagnostics
-        background = false, -- use background color for virtual text
-    },
+    lualine = { transparent = true },
+    diagnostics = { darker = true, undercurl = true, background = false },
 })
-
 onedark.load()
 
 -- transparent background
-vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
-vim.api.nvim_set_hl(0, "FloatBorder", {bg = "none"})
-vim.api.nvim_set_hl(0, "Pmenu", {bg = "none"})
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
