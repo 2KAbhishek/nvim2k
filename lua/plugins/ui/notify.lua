40c8aa397a20b7640b1beebd5d1c76b335d1454c
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-03-08 01:49:32 +0530

    feat: dynamic notification size

--- lua/plugins/ui/notify.lua
+++ lua/plugins/ui/notify.lua
@@ -16,19 +16,26 @@ notify.setup({
         WARN = icons.diagnostics.Warning,
     },
     level = 2,
-    minimum_width = 50,
-    max_height = 10,
-    max_width = 100,
     render = 'default',
     stages = 'fade_in_slide_out',
     timeout = 2000,
     top_down = true,
+
+    max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+    end,
+    max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+    end,
+    on_open = function(win)
+        vim.api.nvim_win_set_config(win, { zindex = 100 })
+    end,
 })
 
 -- Controls noisy notifications
 local buffered_messages = {
     'Client %d+ quit',
-    'No node found at cursor'
+    'No node found at cursor',
 }
 
 local message_notifications = {}
