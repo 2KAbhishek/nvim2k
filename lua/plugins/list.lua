f537fed5567cdf414d150bbf51993f5be43c5177
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-03-08 01:23:01 +0530

    fix: cleanup cmp config

--- lua/plugins/list.lua
+++ lua/plugins/list.lua
@@ -172,7 +172,6 @@ local plugins = {
             'hrsh7th/cmp-path',
             'hrsh7th/cmp-cmdline',
             'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-nvim-lsp-signature-help',
             'hrsh7th/cmp-nvim-lua',
             'saadparwaiz1/cmp_luasnip',
         },
