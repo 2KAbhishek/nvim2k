0bc1830c38b59250c472b6f23fc8848863ac33e6
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-03-07 17:07:03 +0530

    feat: replace indentline with indent scope

---
+++ lua/plugins/ui/indentscope.lua
@@ -0,0 +1,37 @@
+local status_ok, indent = pcall(require, 'mini.indentscope')
+if not status_ok then
+    return
+end
+indent.setup({
+  draw = {
+    delay = 100,
+  },
+
+  mappings = {
+    -- Textobjects
+    object_scope = 'ii',
+    object_scope_with_border = 'ai',
+
+    -- Motions (jump to respective border line; if not present - body line)
+    goto_top = '[i',
+    goto_bottom = ']i',
+  },
+
+  -- Options which control scope computation
+  options = {
+    -- Type of scope's border: which line(s) with smaller indent to
+    -- categorize as border. Can be one of: 'both', 'top', 'bottom', 'none'.
+    border = 'both',
+
+    -- Whether to use cursor column when computing reference indent.
+    -- Useful to see incremental scopes with horizontal cursor movements.
+    indent_at_cursor = true,
+
+    -- Whether to first check input line to be a border of adjacent scope.
+    -- Use it if you want to place cursor on function header to get scope of
+    -- its body.
+    try_as_border = true,
+  },
+
+  symbol = '|',
+})
