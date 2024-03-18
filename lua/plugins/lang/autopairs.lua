2114b5c370cf0585226213f4e0da8f1004985f30
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-03-18 19:37:18 +0530

    fix: remove pcall

--- lua/plugins/lang/autopairs.lua
+++ lua/plugins/lang/autopairs.lua
@@ -22,8 +22,5 @@ npairs.setup({
 })
 
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp_status_ok, cmp = pcall(require, 'cmp')
-if not cmp_status_ok then
-    return
-end
+local cmp = require('cmp')
 cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
