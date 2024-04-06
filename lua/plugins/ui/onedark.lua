f000f998c68ce899e0d2a1e9eb12255c6b3af2ba
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-04-06 12:02:00 +0530

    feat: add highlight colors search, line, visual
    IncSearch changes yank color

--- lua/plugins/ui/onedark.lua
+++ lua/plugins/ui/onedark.lua
@@ -29,3 +29,15 @@ vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
+
+-- set hlsearch color
+vim.api.nvim_set_hl(0, 'Search', { bg = '#8BCD5B', fg = '#202020' })
+vim.api.nvim_set_hl(0, 'CurSearch', { bg = '#EFBD5D', fg = '#000000' })
+vim.api.nvim_set_hl(0, 'IncSearch', { bg = '#F15664', fg = '#000000' })
+
+-- set cursorline color
+vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#1A1A1F' })
+vim.api.nvim_set_hl(0, 'CursorColumn', { bg = '#1A1A1F' })
+
+-- set visual highlight
+vim.api.nvim_set_hl(0, 'Visual', { bg = '#103070' })
