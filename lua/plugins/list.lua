85c14eb4b9e4e4268c917aab2f14b3fb9a592c7d
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-07-01 00:16:03 +0530

    feat: add navigator previous bindings

--- lua/plugins/list.lua
+++ lua/plugins/list.lua
@@ -309,13 +309,14 @@ local plugins = {
     {
         '2kabhishek/termim.nvim',
         cmd = { 'Fterm', 'FTerm', 'Sterm', 'STerm', 'Vterm', 'VTerm' },
+        -- dir = '~/Projects/GitHub/Maintain/termim.nvim',
     },
     {
         '2kabhishek/tdo.nvim',
         dependencies = 'nvim-telescope/telescope.nvim',
         cmd = { 'Tdo', 'TdoEntry', 'TdoNote', 'TdoTodos', 'TdoToggle', 'TdoFind', 'TdoFiles' },
         keys = { '[t', ']t' },
-        -- dir = "~/Projects/GitHub/Maintain/tdo.nvim"
+        -- dir = '~/Projects/GitHub/Maintain/tdo.nvim',
     },
     {
         'kndndrj/nvim-dbee',
