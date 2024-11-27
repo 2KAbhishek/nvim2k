7958050c313f36b48ebae8ab06eeac5aefb31dde
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-11-27 19:36:30 +0530

    feat: add default lang to exercism

--- lua/plugins/tools/exercism.lua
+++ lua/plugins/tools/exercism.lua
@@ -3,4 +3,5 @@ local exercism = require('exercism')
 exercism.setup({
     exercism_workspace = '~/Projects/2KAbhishek/exercism',
     add_default_keybindings = true,
+    default_language = 'elixir',
 })
