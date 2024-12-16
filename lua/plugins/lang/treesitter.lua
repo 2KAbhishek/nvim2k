f88fe0bb13377883e7cbf0260a2e0964e26cd3f3
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-12-16 10:00:10 +0530

    feat: disable treesitter on large files

--- lua/plugins/lang/treesitter.lua
+++ lua/plugins/lang/treesitter.lua
@@ -22,7 +22,13 @@ treesitter.setup({
 
     highlight = {
         enable = true,
-        disable = {},
+        disable = function(lang, buf)
+            local max_filesize = 50 * 1024 -- 50 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            end
+        end,
         additional_vim_regex_highlighting = false,
     },
 
