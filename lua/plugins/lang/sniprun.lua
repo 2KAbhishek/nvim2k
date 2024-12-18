e65d1c4dc797688c7692147a9c4d4cb610d8fa51
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-12-18 11:09:55 +0530

    feat: config snack.scratch

--- lua/plugins/lang/sniprun.lua
+++ lua/plugins/lang/sniprun.lua
@@ -1,7 +1,8 @@
 local sniprun = require('sniprun')
+local sniprun_api = require('sniprun.api')
 
 sniprun.setup({
-    display = { 'VirtualTextOk', 'VirtualTextErr' },
+    display = { 'Api' },
     show_no_output = { 'Classic', 'TempFloatingWindow' },
 
     display_options = {
@@ -12,3 +13,15 @@ sniprun.setup({
     inline_messages = 0,
     borders = 'single',
 })
+
+local api_listener = function(d)
+    if d.status == 'ok' then
+        vim.notify('Output: \n' .. d.message, vim.log.levels.INFO)
+    elseif d.status == 'error' then
+        vim.notify('Error: \n' .. d.message, vim.log.levels.ERROR)
+    else
+        vim.notify('Sniprun: ' .. d.message, vim.log.levels.INFO)
+    end
+end
+
+sniprun_api.register_listener(api_listener)
--- lua/plugins/tools/snacks.lua
+++ lua/plugins/tools/snacks.lua
@@ -144,6 +144,70 @@ Snacks.setup({
             },
         },
     },
+    scratch = {
+        enabled = true,
+        name = 'Scratch',
+        ft = function()
+            if vim.bo.buftype == '' and vim.bo.filetype ~= '' then
+                return vim.bo.filetype
+            end
+            return 'markdown'
+        end,
+        icon = nil,
+        root = vim.fn.stdpath('data') .. '/scratch',
+        autowrite = true,
+        filekey = {
+            cwd = true,
+            branch = true,
+            count = true,
+        },
+        win = {
+            width = 120,
+            height = 40,
+            bo = { buftype = '', buflisted = false, bufhidden = 'hide', swapfile = false },
+            minimal = false,
+            noautocmd = false,
+            zindex = 20,
+            wo = { winhighlight = 'NormalFloat:Normal' },
+            border = 'rounded',
+            title_pos = 'center',
+            footer_pos = 'center',
+
+            keys = {
+                ['execute'] = {
+                    '<cr>',
+                    function(_)
+                        vim.cmd('%SnipRun')
+                    end,
+                    desc = 'Execute buffer',
+                    mode = { 'n', 'x' },
+                },
+            },
+        },
+        win_by_ft = {
+            lua = {
+                keys = {
+                    ['source'] = {
+                        '<cr>',
+                        function(self)
+                            local name = 'scratch.' .. vim.fn.fnamemodify(vim.api.nvim_buf_get_name(self.buf), ':e')
+                            Snacks.debug.run({ buf = self.buf, name = name })
+                        end,
+                        desc = 'Source buffer',
+                        mode = { 'n', 'x' },
+                    },
+                    ['execute'] = {
+                        'e',
+                        function(_)
+                            vim.cmd('%SnipRun')
+                        end,
+                        desc = 'Execute buffer',
+                        mode = { 'n', 'x' },
+                    },
+                },
+            },
+        },
+    },
     zen = {
         enabled = true,
         toggles = {
