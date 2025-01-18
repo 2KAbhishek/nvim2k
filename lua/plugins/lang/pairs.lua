e606a2a62a678bcf9c7f9bdfcf4e841b24839abd
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-01-19 02:24:15 +0530

    docs: remove comments

--- lua/plugins/lang/pairs.lua
+++ lua/plugins/lang/pairs.lua
@@ -1,16 +1,7 @@
 local pairs = require('mini.pairs')
--- No need to copy this inside `setup()`. Will be used automatically.
-pairs.setup({
-    -- In which modes mappings from this `config` should be created
-    modes = { insert = true, command = true, terminal = false },
 
-    -- Global mappings. Each right hand side should be a pair information, a
-    -- table with at least these fields (see more in |MiniPairs.map|):
-    -- - <action> - one of 'open', 'close', 'closeopen'.
-    -- - <pair> - two character string for pair to be used.
-    -- By default pair is not inserted after `\`, quotes are not recognized by
-    -- `<CR>`, `'` does not insert pair after a letter.
-    -- Only parts of tables can be tweaked (others will use these defaults).
+pairs.setup({
+    modes = { insert = true, command = true, terminal = false },
     mappings = {
         ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
         ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
