850d87b7f6183d62647cc48380dd21c0be11acf2
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-03-16 19:18:37 +0530

    feat: add lualine comp for codecompanion

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -57,6 +57,54 @@ local conditions = {
         return gitdir and #gitdir > 0 and #gitdir < #filepath
     end,
 }
+
+local codecompanion = require('lualine.component'):extend()
+codecompanion.processing = false
+codecompanion.spinner_index = 1
+
+local spinner_symbols = {
+    '⠋',
+    '⠙',
+    '⠹',
+    '⠸',
+    '⠼',
+    '⠴',
+    '⠦',
+    '⠧',
+    '⠇',
+    '⠏',
+}
+local spinner_symbols_len = 10
+
+-- Initializer
+function codecompanion:init(options)
+    codecompanion.super.init(self, options)
+
+    local group = vim.api.nvim_create_augroup('CodeCompanionHooks', {})
+
+    vim.api.nvim_create_autocmd({ 'User' }, {
+        pattern = 'CodeCompanionRequest*',
+        group = group,
+        callback = function(request)
+            if request.match == 'CodeCompanionRequestStarted' then
+                self.processing = true
+            elseif request.match == 'CodeCompanionRequestFinished' then
+                self.processing = false
+            end
+        end,
+    })
+end
+
+-- Function that runs every time statusline is updated
+function codecompanion:update_status()
+    if self.processing then
+        self.spinner_index = (self.spinner_index % spinner_symbols_len) + 1
+        return spinner_symbols[self.spinner_index]
+    else
+        return nil
+    end
+end
+
 local searchcount = { 'searchcount', color = { fg = colors.fg, gui = 'bold' } }
 local selectioncount = { 'selectioncount', color = { fg = colors.fg, gui = 'bold' } }
 local progress = { 'progress', color = { fg = colors.fg, gui = 'bold' } }
@@ -197,7 +245,7 @@ lualine.setup({
         lualine_a = {},
         lualine_b = {},
         lualine_c = { separator, mode(icons.ui.Heart), 'location', progress, filename },
-        lualine_x = { diagnostics, lsp, filetype, filesize, fileformat, encoding, separator },
+        lualine_x = { codecompanion, diagnostics, lsp, filetype, filesize, fileformat, encoding, separator },
         lualine_y = {},
         lualine_z = {},
     },
