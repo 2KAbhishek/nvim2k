847aac8ca367655d5e65ca02c16a21fcf0b6d1bd
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-03-30 19:49:39 +0530

    fix: potential fix for lua ls server
    notification is still broken

--- lua/plugins/lang/lspconfig.lua
+++ lua/plugins/lang/lspconfig.lua
@@ -35,7 +35,8 @@ require('mason-lspconfig').setup({
                     Lua = {
                         runtime = { version = 'LuaJIT' },
                         diagnostics = { globals = { 'vim' } },
-                        workspace = { library = { vim.env.VIMRUNTIME } },
+                        workspace = { library = vim.api.nvim_get_runtime_file('', true) },
+                        telemetry = { enable = false },
                         format = {
                             enable = true,
                             defaultConfig = {
