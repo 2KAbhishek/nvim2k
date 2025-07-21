8c8fde79f022b0e831cfedec26279832e6ca2ece
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-07-22 01:13:44 +0530

    feat: update octohub configs

--- lua/plugins/tools/octohub.lua
+++ lua/plugins/tools/octohub.lua
@@ -1,19 +1,30 @@
 local octohub = require('octohub')
 
 octohub.setup({
-    per_user_dir = true,
-    projects_dir = '~/Projects/',
-    contribution_icons = { '', '', '', '', '', '', '' },
-    sort_repos_by = '',
-    repo_type = '',
-    max_contributions = 50,
-    top_lang_count = 5,
-    event_count = 5,
-    window_width = 90,
-    window_height = 60,
-    show_recent_activity = true,
-    show_contributions = true,
-    show_repo_stats = true,
+    repos = {
+        per_user_dir = true,
+        projects_dir = '~/Projects/',
+        sort_by = '',
+        repo_type = '',
+    },
+    stats = {
+        contribution_icons = { '', '', '', '', '', '', '' },
+        max_contributions = 50,
+        top_lang_count = 5,
+        event_count = 5,
+        window_width = 90,
+        window_height = 60,
+        show_recent_activity = true,
+        show_contributions = true,
+        show_repo_stats = true,
+    },
+    cache = {
+        events = 3600 * 6,
+        contributions = 3600 * 6,
+        repos = 3600 * 24 * 7,
+        username = 3600 * 24 * 7,
+        user = 3600 * 24 * 7,
+    },
     add_default_keybindings = true,
     use_new_command = true,
 })
