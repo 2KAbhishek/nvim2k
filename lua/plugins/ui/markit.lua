6a2ced8239233fc0b149d0dec49b8c4fec0f9eea
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-08-08 05:19:08 +0530

    feat: cleanup markit configs

--- lua/plugins/ui/markit.lua
+++ lua/plugins/ui/markit.lua
@@ -2,36 +2,17 @@ local markit = require('markit')
 local icons = require('lib.icons')
 
 markit.setup({
-    default_mappings = true,
+    add_default_keybindings = true,
     builtin_marks = { '.', '<', '>', '^' },
     cyclic = true,
     force_write_shada = false,
     sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
     excluded_filetypes = {},
     excluded_buftypes = { 'nofile' },
-    -- supports up to 10 bookmark groups
-    bookmark_1 = {
-        sign = icons.ui.Flag,
-        virt_text = 'flag',
-        annotate = false,
-    },
-    bookmark_2 = {
-        sign = icons.ui.Eye,
-        virt_text = 'watch',
-        annotate = false,
-    },
-    bookmark_3 = {
-        sign = icons.ui.Star,
-        virt_text = 'star',
-        annotate = false,
-    },
-    bookmark_4 = {
-        sign = icons.ui.Bug,
-        virt_text = 'bug',
-        annotate = false,
-    },
-    mappings = {
-        set = 'M',
-        toggle_mark = 'm',
+    bookmarks = {
+        { sign = icons.ui.Neovim, virt_text = 'flag', annotate = false },
+        { sign = icons.ui.Eye, virt_text = 'watch', annotate = false },
+        { sign = icons.ui.Star, virt_text = 'star', annotate = false },
+        { sign = icons.ui.Bug, virt_text = 'bug', annotate = false },
     },
 })
