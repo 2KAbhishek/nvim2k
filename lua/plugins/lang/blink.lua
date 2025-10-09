cfea2ea0674ffefb0e4fb2db08a0920e8a659482
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-10-09 08:41:32 +0530

    feat: add sidekick next edit suggestion to completion

--- lua/plugins/lang/blink.lua
+++ lua/plugins/lang/blink.lua
@@ -5,7 +5,14 @@ blink.setup({
     -- 'default', 'super-tab', 'enter'
     keymap = {
         preset = 'enter',
-        ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+        ["<Tab>"] = {
+            function()
+                return require("sidekick").nes_jump_or_apply()
+            end,
+            'select_next',
+            'snippet_forward',
+            'fallback',
+        },
         ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
         ['<C-space>'] = { 'show', 'hide' },
         ['<C-y>'] = { 'show_documentation', 'hide_documentation' },
