abe9d7f8f9de5fabf93d73661216361226b7997b
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-06-12 11:48:18 +0530

    feat: remove logic in ts - snacks.bigfile does it

--- lua/plugins/editor/treesitter.lua
+++ lua/plugins/editor/treesitter.lua
@@ -13,20 +13,6 @@ if auto_install then
     end, 0)
 end
 
--- Centralized Tree-sitter Highlighting limit for large files
-local ok, ts_start = pcall(function() return vim.treesitter.start end)
-if ok and type(ts_start) == 'function' then
-    vim.treesitter.start = function(bufnr, lang)
-        bufnr = bufnr or vim.api.nvim_get_current_buf()
-        local max_filesize = 500 * 1024 -- 500 KB limit
-        local name = vim.api.nvim_buf_get_name(bufnr)
-        local ok_stat, stats = pcall(vim.loop.fs_stat, name)
-        if ok_stat and stats and stats.size > max_filesize then
-            return
-        end
-        ts_start(bufnr, lang)
-    end
-end
 
 -- Enable Treesitter-based Indentation for buffers with a parser
 vim.api.nvim_create_autocmd('FileType', {
