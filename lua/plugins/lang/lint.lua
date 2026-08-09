6420e34c165b943194bb21a72c0717151be4afd7
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-08-10 03:11:37 +0530

    fix(lint): run try_lint only on normal file buffers

--- lua/plugins/lang/lint.lua
+++ lua/plugins/lang/lint.lua
@@ -14,6 +14,8 @@ lint.linters_by_ft = {
 vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost', 'InsertLeave' }, {
     group = vim.api.nvim_create_augroup('nvim2k_lint', { clear = true }),
     callback = function()
-        lint.try_lint()
+        if vim.bo.buftype == '' then
+            pcall(lint.try_lint)
+        end
     end,
 })
