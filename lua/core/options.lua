18f36e322cd667120b9ad73071d3b3f5fabb4f61
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-08-10 01:22:17 +0530

    perf(core): disable legacy remote plugin providers

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -71,6 +71,12 @@ vim.g.netrw_liststyle = 1
 -- Fix markdown indentation settings
 vim.g.markdown_recommended_style = 0
 
+-- Disable legacy provider checks
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
+
 vim.opt.path:append({ '**' })
 vim.opt.shortmess:append({ W = true, I = true, c = true })
 
